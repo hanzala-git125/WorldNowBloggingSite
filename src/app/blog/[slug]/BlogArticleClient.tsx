3ee8e846ded0ea83252868b66ebe9f8c6df1a6ba
gitking-han <hanzalarehman1000@gmail.com>
2026-09-24 21:27:29 +0500

    commit43

--- src/app/blog/[slug]/BlogArticleClient.tsx
+++ src/app/blog/[slug]/BlogArticleClient.tsx
@@ -9,9 +9,9 @@ import api from '@/utils/api';
 import { renderMarkdown } from '@/utils/markdown';
 import { Blog, Comment } from '@/types';
 import ArticleCard from '@/components/ArticleCard';
-import AdsterraResponsiveBanner from '@/components/AdsterraResponsiveBanner';
-import AdsterraArticleContent from '@/components/AdsterraArticleContent';
-import AdsterraNative from "@/components/AdsterraNative";
+// import AdsterraResponsiveBanner from '@/components/AdsterraResponsiveBanner';
+// import AdsterraArticleContent from '@/components/AdsterraArticleContent';
+// import AdsterraNative from "@/components/AdsterraNative";
 
 export default function BlogArticleClient({ initialBlog }: { initialBlog?: Blog | null }) {
   const params = useParams();
@@ -182,9 +182,9 @@ export default function BlogArticleClient({ initialBlog }: { initialBlog?: Blog 
                 {blog.excerpt}
               </p>
             )}
-            <div className="prose prose-lg max-w-none font-serif text-gray-800 leading-relaxed">
+            {/* <div className="prose prose-lg max-w-none font-serif text-gray-800 leading-relaxed">
               <AdsterraArticleContent html={renderedContent} articleKey={slug} />
-            </div>
+            </div> */}
           </div>
 
           {blog.tags && blog.tags.length > 0 && (
@@ -301,8 +301,8 @@ export default function BlogArticleClient({ initialBlog }: { initialBlog?: Blog 
             </div>
           </section>
         </article>
-        <AdsterraNative />
-        <AdsterraResponsiveBanner />
+        {/* <AdsterraNative />
+        <AdsterraResponsiveBanner /> */}
         {relatedBlogs.length > 0 && (
           <div className="mt-16">
             
